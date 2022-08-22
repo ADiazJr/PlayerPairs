cff831c5daf94a4daf28a50840feab19783e580e
unknown <arturodiazmay@gmail.com>
2022-08-22 15:15:48 -0500

    feat. added deal_cards() function that makes players and hands them each 5 cards moved the shuffling of the deck to randmoize_deck() function

--- main.py
+++ main.py
@@ -11,9 +11,28 @@ def RunGame():
                 deck.append(item)
                 amount_per_type += 1
         return deck
-    
     game_deck = CreateDeck()
-    random.shuffle(game_deck)
-    print(game_deck)
+    
+    def randomize_deck(created_deck):
+        random.shuffle(created_deck)
+        return created_deck
 
+    shuffled_deck = randomize_deck(game_deck)
+
+    def deal_cards(shuffled_deck):
+        player_one = []
+        player_two = []
+        player_three = []
+        player_four = []
+        players = [player_one, player_two, player_three, player_four]
+        deal_number = 1
+        while deal_number != 21:
+            for player in players:
+                card = shuffled_deck.pop(1)
+                player.append(card)
+                print(player)
+                deal_number += 1
+        return (players)
+    players = deal_cards(shuffled_deck)
+    print(players)
 RunGame()
